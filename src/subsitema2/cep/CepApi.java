95c56fefd16c4ec07d202b7717656be70ce08387
unknown <leandrods.ads@gmail.com>
2022-05-24 12:18:50 -0300

    Expert Talk 24/05/2022

---
+++ src/subsitema2/cep/CepApi.java
@@ -0,0 +1,23 @@
+package subsitema2.cep;
+
+public class CepApi {
+	
+private static CepApi instancia = new CepApi();
+	
+	private CepApi() {
+		super();
+	}
+	
+	public static CepApi getInstancia() {
+		return instancia;
+	}
+	
+	public String recuperarCidade(String cep) {
+		return "Luziania";
+	}
+	
+	public String recuperarEstado(String cep) {
+		return "Goias";
+	}
+
+}
